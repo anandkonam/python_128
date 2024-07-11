36a9503d10601eb7b212c2f478b63e2b81c6abbe
anandkonam <anilk46629@gmail.com>
2024-07-11 21:14:15 +0530

    first commit

---
+++ multiplication_table.py
@@ -0,0 +1,3 @@
+n1=int(input("enter a number"))
+for i in range(1,11):
+    print(n1,' * ',i,'=',n1*i)
---
+++ triangle_demo.py
@@ -0,0 +1,14 @@
+for i in range(1,5+1):
+    for j in range(1,i+1):
+        print(' * ',end=" ")
+    print()    
+
+for i in range(0,5,1):
+    #for j in range(1,i+1):
+        print(' * ' *i)
+print("   ")        
+
+for i in range(5,0,-1):
+    print(' *' *i)        
+       
+      
